07f3e1eeb83784eb36da9cc151c03eca665716ec
Shivaraj K M <Shivaraj K M@DESKTOP-PK5SA4L>
2023-02-05 14:34:09 +0530

    commit1

---
+++ src/test/java/practice/DemoTest.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void demoTest() {
+		Reporter.log("hiii",true);
+	}
+}
